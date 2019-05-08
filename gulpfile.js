c911a0035b46568a82d591244764fedf8f52c2a1
Dinesh Don <dinesh.opatha@gmail.com>
2019-05-08 17:40:29 -0400

    update sass,ts paths in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,17 @@ const browserSync = require('browser-sync').create();
 
 // compile scss into css
 function style(){
-    return gulp.src('./src/styles/scss/**/*.scss')
+    return gulp.src('./scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./src/styles/css'))
+        .pipe(gulp.dest('./src/css'))
         .pipe(browserSync.stream());
 }
 
 function tScripts(){
-    return gulp.src('./src/scripts/ts/**/*.ts')
+    return gulp.src('./typescript/**/*.ts')
         .pipe(ts())
         .on('error', () => { /* Ignore compiler errors */})
-        .pipe(gulp.dest('./src/scripts/js/'))
+        .pipe(gulp.dest('./src/javascript/'))
         .pipe(browserSync.stream());
 }
 
@@ -26,8 +26,8 @@ function watch(){
         }
     });
 
-    gulp.watch('./src/styles/scss/**/*.scss', style);
-    gulp.watch('./src/scripts/ts/**/*.ts', tScripts);
+    gulp.watch('./scss/**/*.scss', style);
+    gulp.watch('./typescript/**/*.ts', tScripts);
     gulp.watch('./**/*.html').on('change',browserSync.reload);
 }
 
